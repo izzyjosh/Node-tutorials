6c478ae007e5229642f88ba24ca9b787d4c0006f
izzyjosh <calebayuba222@gmail.com>
2024-02-18 16:18:02 +0100

    practicing node.js

--- file.js
+++ file.js
@@ -10,16 +10,54 @@ readFile("./text.txt","utf8",(err,success) => {
 
 console.log("checking for file...")
 
-async function writeFile("./writing.txt","i am writing to this file",(err,success) => {
-  if (err) {
+
+//promise can also be written using the util module in nodejs threough the steps below
+//
+//const util = require("util")
+//
+//const getText = util.promisify(readFile)
+//const writeNewText = util.promisify(writeFile)
+
+
+const getText = (path) => {
+  return new Promise((resolve,reject) => {
+    readFile(path,"utf8",(err,data) => {
+      if (err) {
+        reject(err)
+      }
+      else {
+        resolve(data)
+      }
+    })
+  })
+}
+
+const newWriteFile = (path,note) => {
+  return new Promise((resolve,reject) => {
+    writeFile(path,note,(err,success) => {
+      if (err) {
+        reject(err)
+      }
+      else {
+        resolve(success)
+      }
+    })
+  })
+}
+
+const start = async() => {
+  try {
+    const firstFile = await getText("./text.txt")
+    const secondFile = await getText("./writing.txt")
+    const note = `${firstFile} ${secondFile}`
+    const path = "./check.txt"
+    const write = await newWriteFile(path,note)
+    const getWrite = await getText(path)
+    console.log(getWrite)
+  }
+  catch(err) {
     console.log(err)
   }
-  else {
-    readFile("./writing.txt","utf8",(err,success) => {
-      if (err) {
-        console.log(err)
-      }
-      console.log(success)
-    })
-  }
-})
+}
+
+start()
--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.get("/",(req,res) => {
   res.send("Hello world")
 })
 
-introduce.introduce("joshua")
+introduce.introduce("joshua Joseph")
 
 app.listen(3000,() => {
   console.log("Express running on port 3000")
